bd1f30854bcfa20ef8e777d4604fd6ff089af6a9
Felix Quast <51856713+felix-oq@users.noreply.github.com>
2021-10-13 11:53:23 +0200

    Use alternative option to don't start dependent services Uses --no-deps instead of multiple --scale options

--- storage/storage-mq/cdct-provider.sh
+++ storage/storage-mq/cdct-provider.sh
@@ -5,4 +5,4 @@ set -e
 
 dir=$(dirname "$0")
 docker-compose -f "${dir}/../../docker-compose.yml" -f "${dir}/../../docker-compose.provider.yml" build storage storage-db storage-mq
-docker-compose -f "${dir}/../../docker-compose.yml" -f "${dir}/../../docker-compose.provider.yml" up --scale rabbitmq=0 --scale storage-db-liquibase=0 --exit-code-from storage-mq storage storage-db storage-mq
+docker-compose -f "${dir}/../../docker-compose.yml" -f "${dir}/../../docker-compose.provider.yml" up --exit-code-from storage-mq --no-deps storage storage-db storage-mq
