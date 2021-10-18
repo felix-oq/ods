4ab8f5b2749c4a7b6bd1d058d2278e4ca48b0594
Felix Quast <51856713+felix-oq@users.noreply.github.com>
2021-10-18 10:48:02 +0200

    Make CDCT script executable from any directory

--- storage/storage-mq/cdct-provider.sh
+++ storage/storage-mq/cdct-provider.sh
@@ -4,5 +4,6 @@
 set -e
 
 dir=$(dirname "$0")
-docker-compose -f "${dir}/../../docker-compose.yml" -f "${dir}/../../docker-compose.provider.yml" build storage storage-db storage-mq
-docker-compose -f "${dir}/../../docker-compose.yml" -f "${dir}/../../docker-compose.provider.yml" up --exit-code-from storage-mq --no-deps storage storage-db storage-mq
+cd ${dir}/../..
+docker-compose -f docker-compose.yml -f docker-compose.provider.yml build storage storage-db storage-mq
+docker-compose -f docker-compose.yml -f docker-compose.provider.yml up --exit-code-from storage-mq --no-deps storage storage-db storage-mq
