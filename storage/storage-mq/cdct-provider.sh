ec37e5ea325d15a322171bf0c2e416bbd7b8d86e
Felix Quast <51856713+felix-oq@users.noreply.github.com>
2021-10-18 17:02:46 +0200

    Run liquibase before storage provider test Without running liquibase first, the required database users are not registered.

--- storage/storage-mq/cdct-provider.sh
+++ storage/storage-mq/cdct-provider.sh
@@ -5,5 +5,6 @@ set -e
 
 dir=$(dirname "$0")
 cd ${dir}/../..
-docker-compose -f docker-compose.yml -f docker-compose.provider.yml build storage storage-db storage-mq
+docker-compose -f docker-compose.yml -f docker-compose.provider.yml build storage storage-db storage-mq storage-db-liquibase
+docker-compose -f docker-compose.yml -f docker-compose.provider.yml up --exit-code-from storage-db-liquibase storage-db storage-db-liquibase
 docker-compose -f docker-compose.yml -f docker-compose.provider.yml up --exit-code-from storage-mq --no-deps storage storage-db storage-mq
